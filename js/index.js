959ab6e4bc7710e69d9487486cb94cfbcdf4f1c3
sophia0110 <njjp6ru@yahoo.com.tw>
2022-11-06 13:53:53 +0800

    Parallax Scrolling

--- js/index.js
+++ js/index.js
@@ -1,4 +1,134 @@
 gsap.registerPlugin(ScrollTrigger, TextPlugin);
-gsap.to('.newspaper_1', {y: -800, duration:1, delay: 1})
 
-gsap.to('.img_people', {y: -440, duration:1, delay: 1})
+const img_people = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".img_people ",
+    start: "top 75%",
+    end: "top 1%",
+    endTrigger: "#introduce",
+    scrub: true,
+  },
+});
+
+img_people
+  .to(".img_people", {
+    top: "100%",
+    yPercent: "-100",
+    opacity: 1,
+
+  })
+
+const newspaper_1 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".newspaper_1 ",
+    start: "top 45%",
+    scrub: true,
+  },
+});
+
+newspaper_1
+  .to(".newspaper_1", {
+    top: "100%",
+    yPercent: "0",
+    opacity: 1,
+  })
+  .to(".newspaper_1", {
+    top: "0",
+    yPercent: "-300",
+    opacity: 0,
+  });
+
+const newspaper_2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".newspaper_2 ",
+    start: "top 45%",
+    end: "top 1%",
+    scrub: true,
+  },
+});
+
+newspaper_2
+  .to(".newspaper_2 ", {
+    top: 0,
+    yPercent: "0",
+    opacity: 1,
+  })
+  .to(".newspaper_2", {
+    top: "100%",
+    yPercent: "200",
+    opacity: 0,
+  });
+
+const newspaper_3 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".newspaper_3 ",
+    start: "top 45%",
+    end: "top 1%",
+    scrub: true,
+  },
+});
+
+newspaper_3
+  .to(".newspaper_3 ", {
+    top: 0,
+    yPercent: "0",
+    opacity: 1,
+  })
+  .to(".newspaper_3", {
+    top: "100%",
+    yPercent: "200",
+    opacity: 0,
+  });
+
+
+  const card_1 = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#card_1",
+      start: 'top 100%',
+      end: 'top 1%',
+      scrub: true,
+    },
+  })
+
+  card_1.to('#card_1', {
+    top: 0,
+    xPercent: '-200',
+  }).to('#card_1', {
+    top: 0,
+    xPercent: '0',
+  })
+
+
+  const card_3 = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#card_3",
+      start: 'top 100%',
+      end: 'top 1%',
+      scrub: true,
+    },
+  })
+
+  card_3.to('#card_3', {
+    top: 0,
+    xPercent: '-200',
+  }).to('#card_3', {
+    top: 0,
+    xPercent: '0',
+  })
+
+  const card_2 = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#card_2",
+      start: 'top 100%',
+      end: 'top 1%',
+      scrub: true,
+    },
+  })
+
+  card_2.to('#card_2', {
+    top: 0,
+    xPercent: '200',
+  }).to('#card_2', {
+    top: 0,
+    xPercent: '0',
+  })
