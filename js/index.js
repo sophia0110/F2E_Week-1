a8f8c02a77939408d1334e1356090d13598c4bf8
sophia0110 <njjp6ru@yahoo.com.tw>
2022-11-09 23:06:49 +0800

    add mouse animation

--- js/index.js
+++ js/index.js
@@ -1,21 +1,36 @@
 gsap.registerPlugin(ScrollTrigger, TextPlugin);
 
-const img_people = gsap.timeline({
+const mouse = gsap.timeline({
   scrollTrigger: {
-    trigger: ".img_people ",
-    start: "top 25%",
+    trigger: "#mouse",
+    start: "top 3%",
     end: "top 1%",
     scrub: true,
   },
 });
 
-img_people.to(".img_people", {
-  bottom: "70%",
-  yPercent: "-48",
-}).to(".img_people", {
-  bottom: "50%",
-  yPercent: "-68",
+mouse.to("#mouse", {
+  visibility: "hidden",
 });
+
+const img_people = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".img_people ",
+    start: "top 35%",
+    end: "top 1%",
+    scrub: true,
+  },
+});
+
+img_people
+  .to(".img_people", {
+    bottom: "30%",
+    yPercent: "-48",
+  })
+  .to(".img_people", {
+    bottom: "0%",
+    yPercent: "-68",
+  });
 
 const newspaper_1 = gsap.timeline({
   scrollTrigger: {
@@ -32,7 +47,8 @@ newspaper_1
     top: "100%",
     yPercent: "-80",
     opacity: 1,
-  }) .to(".newspaper_1", {
+  })
+  .to(".newspaper_1", {
     top: "30%",
     yPercent: "-300",
     opacity: 1,
@@ -58,26 +74,15 @@ newspaper_2
     yPercent: "0",
     opacity: 1,
   })
-  .to(".newspaper_2", {
-    top: "100%",
-    yPercent: "200",
-    opacity: 0,
-  });
-
-const newspaper_3 = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".newspaper_3 ",
-    start: "top 45%",
-    end: "top 1%",
-    scrub: true,
-  },
-});
-
-newspaper_3
   .to(".newspaper_3 ", {
     top: 0,
     yPercent: "0",
     opacity: 1,
+  })
+  .to(".newspaper_2", {
+    top: "100%",
+    yPercent: "200",
+    opacity: 0,
   })
   .to(".newspaper_3", {
     top: "100%",
@@ -85,22 +90,22 @@ newspaper_3
     opacity: 0,
   });
 
-const mouse = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".mouse",
-    start: "top 20%",
-    end: "top 1%",
-    scrub: true,
-  },
-});
+// const mouse2 = gsap.timeline({
+//   scrollTrigger: {
+//     trigger: ".mouse",
+//     start: "top 20%",
+//     end: "top 1%",
+//     scrub: true,
+//   },
+// });
 
-mouse.to(".mouse", {
-  top: 0,
-  yPercent: "0.05",
-  opacity: 0,
-  duration: 1,
-  repeat: -1,
-});
+// mouse2.to(".mouse", {
+//   top: 0,
+//   yPercent: "0.05",
+//   opacity: 0,
+//   duration: 1,
+//   repeat: -1,
+// });
 
 const card_1 = gsap.timeline({
   scrollTrigger: {
@@ -142,4 +147,20 @@ const card_2 = gsap.timeline({
 card_2.to("#card_2", {
   top: 0,
   left: 0,
+});
+
+const animation = lottie.loadAnimation({
+  container: document.getElementById("loading"), // the dom element that will contain the animation
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "./js/loading.json", // the path to the animation json
+});
+
+const scrollDown = lottie.loadAnimation({
+  container: document.getElementById("mouse"), // the dom element that will contain the animation
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "./js/scroll_down.json", // the path to the animation json
 });
